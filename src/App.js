c9a40a7d594f2855bace86f33f208de8b135f035
sholabioku <bilushonline@gmail.com>
2022-11-28 10:45:57 +0100

    Add setup for routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import "./App.scss";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Contact from './pages/contact/Contact';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!!!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
 };
 
 export default Footer;
