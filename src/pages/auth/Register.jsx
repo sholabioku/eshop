e442fc4ce03d78a08f9ff43305a8cbb5cd357548
sholabioku <bilushonline@gmail.com>
2022-12-09 17:02:32 +0100

    Refracture codes for toastify

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
 
 import Card from '../../components/card/Card';
 import registerImg from '../../assets/register.png';
@@ -21,49 +20,46 @@ const Register = () => {
   };
 
   return (
-    <>
-      <ToastContainer />
-      <section className={`container ${styles.auth}`}>
-        <Card>
-          <div className={styles.form}>
-            <h2>Register</h2>
-            <form onSubmit={registerUser}>
-              <input
-                type='text'
-                placeholder='Email'
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <input
-                type='password'
-                placeholder='Password'
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <input
-                type='password'
-                placeholder='Confirm Password'
-                required
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-              />
-              <button type='submit' className='--btn --btn-primary --btn-block'>
-                Register
-              </button>
-            </form>
-            <span className={styles.register}>
-              <p>Already have an account?</p>
-              <Link to='/login'>Login</Link>
-            </span>
-          </div>
-        </Card>
-        <div className={styles.img}>
-          <img src={registerImg} alt='Register' width='400' />
+    <section className={`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+          <h2>Register</h2>
+          <form onSubmit={registerUser}>
+            <input
+              type='text'
+              placeholder='Email'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type='password'
+              placeholder='Password'
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type='password'
+              placeholder='Confirm Password'
+              required
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button type='submit' className='--btn --btn-primary --btn-block'>
+              Register
+            </button>
+          </form>
+          <span className={styles.register}>
+            <p>Already have an account?</p>
+            <Link to='/login'>Login</Link>
+          </span>
         </div>
-      </section>
-    </>
+      </Card>
+      <div className={styles.img}>
+        <img src={registerImg} alt='Register' width='400' />
+      </div>
+    </section>
   );
 };
 
