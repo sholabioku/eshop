c4252baa41fc65085c5ff4c80eac0d2feb440350
sholabioku <bilushonline@gmail.com>
2022-12-11 11:59:22 +0100

    Add functionality to login user with firebase

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaGoogle } from 'react-icons/fa';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 import loginImg from '../../assets/login.png';
 import styles from './auth.module.scss';
 import Card from '../../components/card/Card';
+import { auth } from '../../firebase/config';
+import { toast } from 'react-toastify';
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState('');
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setIsLoading(false);
+        toast.success('Login Successful...');
+        navigate('/');
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
+  };
+
   return (
     <section className={`container ${styles.auth}`}>
       <div className={styles.img}>
@@ -15,10 +41,24 @@ const Login = () => {
       <Card>
         <div className={styles.form}>
           <h2>Login</h2>
-          <form>
-            <input type='text' placeholder='Email' required />
-            <input type='password' placeholder='Password' required />
-            <button className='--btn --btn-primary --btn-block'>Login</button>
+          <form onSubmit={loginUser}>
+            <input
+              type='text'
+              placeholder='Email'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type='password'
+              placeholder='Password'
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type='submit' className='--btn --btn-primary --btn-block'>
+              Login
+            </button>
             <div className={styles.links}>
               <Link to='/reset'>Reset Password</Link>
             </div>
