bfb5b2ab130bfb486ab87822d5a7a38a4af33768
sholabioku <bilushonline@gmail.com>
2022-12-11 12:02:12 +0100

    Add Loader component

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -8,6 +8,7 @@ import styles from './auth.module.scss';
 import Card from '../../components/card/Card';
 import { auth } from '../../firebase/config';
 import { toast } from 'react-toastify';
+import Loader from '../../components/loader/Loader';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -34,47 +35,50 @@ const Login = () => {
   };
 
   return (
-    <section className={`container ${styles.auth}`}>
-      <div className={styles.img}>
-        <img src={loginImg} alt='Login' width='400' />
-      </div>
-      <Card>
-        <div className={styles.form}>
-          <h2>Login</h2>
-          <form onSubmit={loginUser}>
-            <input
-              type='text'
-              placeholder='Email'
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type='password'
-              placeholder='Password'
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type='submit' className='--btn --btn-primary --btn-block'>
-              Login
-            </button>
-            <div className={styles.links}>
-              <Link to='/reset'>Reset Password</Link>
-            </div>
-            <p>-- or --</p>
-          </form>
-          <button className='--btn --btn-danger --btn-block'>
-            <FaGoogle color='#fff' />
-            Login With Google
-          </button>
-          <span className={styles.register}>
-            <p>Don't have an account?</p>
-            <Link to='/register'>Register</Link>
-          </span>
+    <>
+      {isLoading && <Loader />}
+      <section className={`container ${styles.auth}`}>
+        <div className={styles.img}>
+          <img src={loginImg} alt='Login' width='400' />
         </div>
-      </Card>
-    </section>
+        <Card>
+          <div className={styles.form}>
+            <h2>Login</h2>
+            <form onSubmit={loginUser}>
+              <input
+                type='text'
+                placeholder='Email'
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type='password'
+                placeholder='Password'
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type='submit' className='--btn --btn-primary --btn-block'>
+                Login
+              </button>
+              <div className={styles.links}>
+                <Link to='/reset'>Reset Password</Link>
+              </div>
+              <p>-- or --</p>
+            </form>
+            <button className='--btn --btn-danger --btn-block'>
+              <FaGoogle color='#fff' />
+              Login With Google
+            </button>
+            <span className={styles.register}>
+              <p>Don't have an account?</p>
+              <Link to='/register'>Register</Link>
+            </span>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
